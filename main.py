6ccb4094903345b4b6e165da3718c6319a9f77b3
ypriverol <ypriverol@gmail.com>
2022-02-09 16:59:35 +0000

    small changes for independent samples done

--- main.py
+++ main.py
@@ -2,39 +2,57 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import click
 import pandas as pd
 from pyopenms import *
 
+
+def print_help_msg(command):
+  with click.Context(command) as ctx:
+    click.echo(command.get_help(ctx))
+
+@click.command()
+@click.option("-f", "--fasta", help="Protein database to compute IBAQ values")
+@click.option("-p", "--peptides", help="Peptide identifications with intensities following the triqler output")
+def ibaq_compute( fasta, peptides):
+  if peptides is None or fasta is None:
+    print_help_msg(ibaq_compute)
+    exit(-1)
+
+  fasta_proteins = list()  # type: list[FASTAEntry]
+  FASTAFile().load(fasta, fasta_proteins)
+  uniquepepcounts = dict()  # type: dict[str, int]
+  MINLEN = 6
+  MAXLEN = 30
+  ENZYMENAME = "Trypsin"
+  digestor = ProteaseDigestion()
+  digestor.setEnzyme(ENZYMENAME)
+
+  def getAverageNrUniqPepsForIndistGrp(pdrow):
+    proteins = pdrow.name.split(';')
+    summ = 0
+    for prot in proteins:
+      summ += uniquepepcounts[prot]
+
+    return pdrow.intensity / (summ / len(proteins))
+
+  for entry in fasta_proteins:
+    digest = list()  # type: list[str]
+    digestor.digest(AASequence().fromString(entry.sequence), digest, MINLEN, MAXLEN)
+    digestuniq = set(digest)
+    uniquepepcounts[entry.identifier.decode('utf-8')] = len(digestuniq)
+
+  data = pd.read_csv(peptides, sep="\t")
+  print(data.head())
+  ## next line assumes unique peptides only (at least per indistinguishable group)
+  res = pd.DataFrame(data.groupby('proteins')['intensity'].sum()).apply(getAverageNrUniqPepsForIndistGrp, 1)
+  res.to_csv("res.csv")
+
+
+
 if __name__ == '__main__':
-    fasta = list()  # type: list[FASTAEntry]
-    FASTAFile().load("/Volumes/Data/Homo-sapiens-uniprot-reviewed-isoforms-contaminants-decoy-202105.fasta", fasta)
-    uniquepepcounts = dict()  # type: dict[str, int]
-    MINLEN = 6
-    MAXLEN = 30
-    ENZYMENAME = "Trypsin"
-    digestor = ProteaseDigestion()
-    digestor.setEnzyme(ENZYMENAME)
 
-
-    def getAverageNrUniqPepsForIndistGrp(pdrow):
-        proteins = pdrow.name.split(';')
-        summ = 0
-        for prot in proteins:
-            summ += uniquepepcounts[prot]
-
-        return pdrow.intesity / (summ / len(proteins))
-
-    for entry in fasta:
-        digest = list()  # type: list[str]
-        digestor.digest(AASequence().fromString(entry.sequence), digest, MINLEN, MAXLEN)
-        digestuniq = set(digest)
-        uniquepepcounts[entry.identifier] = len(digestuniq)
-
-    data = pd.read_csv("/Users/pfeuffer/Downloads/out_triqler_Sample1.tsv", sep="\t")
-    print(data.head())
-    ## next line assumes unique peptides only (at least per indistinguishable group)
-    res = pd.DataFrame(data.groupby('proteins')['intensity'].sum()).apply(getAverageNrUniqPepsForIndistGrp, 1)
-    res.to_csv("res.csv")
+  ibaq_compute()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
