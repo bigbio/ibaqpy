6c0d4c983195ceadc39e682e4f6b141fde039947
ypriverol <ypriverol@gmail.com>
2022-02-11 12:01:26 +0000

    small changes for independent samples done

--- test.py
+++
@@ -1,9 +0,0 @@
-from pyopenms import *
-
-if __name__ == '__main__':
-    fasta = list()  # type: list[FASTAEntry]
-    FASTAFile().load("/Volumes/Data/Homo-sapiens-uniprot-reviewed-isoforms-contaminants-decoy-202105.fasta", fasta)
-    uniquepepcounts = dict()  # type: dict[str, int]
-    MINLEN = 6
-    MAXLEN = 30
-    ENZYMENAME = "Trypsin"
