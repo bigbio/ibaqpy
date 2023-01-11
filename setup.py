26c441738c2e5e55bc499ea7aec8febdfd20c269
Yasset Perez-Riverol <ypriverol@gmail.com>
2023-01-11 00:07:56 +0000

    some structure for the package.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'seaborn == 0.10',
         'typing_extensions'
     ],
-    scripts=['bin/compute_ibaq.py', 'bin/peptide_file_generation.py', 'bin/peptide_normalization.py', 'bin/merge_conditional_generation.py'],
+    scripts=['bin/compute_ibaq.py', 'bin/peptide_file_generation.py', 'bin/peptide_normalization.py', 'bin/merge_condition_files.py'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
