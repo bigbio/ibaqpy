222bb627f105a98f46f18d4a528e3f43b35b4a03
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-12-04 12:53:40 +0000

    updates tests.

--- tests/test_ibaqpy.py
+++ tests/test_ibaqpy.py
@@ -1,9 +1,12 @@
 from ibaqpy.bin.peptides2protein import peptides_to_protein
 
+from pathlib import Path
+TESTS_DIR = Path(__file__).parent
+
 def test_ibaq_compute():
     args = {
-        "fasta": "tests/example/Homo-sapiens-uniprot-reviewed-contaminants-decoy-202210.fasta",
-        "peptides": "tests/example/PXD017834-peptides.csv",
+        "fasta": str(TESTS_DIR / "example/Homo-sapiens-uniprot-reviewed-contaminants-decoy-202210.fasta"),
+        "peptides": str(TESTS_DIR / "example/PXD017834-peptides.csv"),
         "enzyme": "Trypsin",
         "normalize": True,
         "min_aa": 7,
@@ -13,9 +16,9 @@ def test_ibaq_compute():
         "ploidy": 2,
         "cpc": 200,
         "organism": "human",
-        "output": "tests/example/PXD017834-norm.csv",
+        "output": str(TESTS_DIR / "example/PXD017834-norm.csv"),
         "verbose": True,
-        "qc_report": "tests/example/QCprofile.pdf",
+        "qc_report": str(TESTS_DIR / "example/QCprofile.pdf"),
     }
     print(args)
     peptides_to_protein(**args)
