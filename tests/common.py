7faf921afd79372f623f5c894b8ef75d020f7171
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-12-04 12:16:52 +0000

    updates tests.

--- tests/common.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-TEST_DATA_ROOT = Path(__file__).parent / "example"
-
-def datafile(path: str):
-    path = str(path)
-    return str(TEST_DATA_ROOT / path)
--- tests/test_ibaqpy.py
+++ tests/test_ibaqpy.py
@@ -1,25 +1,21 @@
-from unittest import TestCase
 from ibaqpy.bin.peptides2protein import peptides_to_protein
-from .common import datafile
 
-class TestIbaqpy(TestCase):
-
-    def test_ibaq_compute(self):
-        args = {
-            "fasta": datafile("Homo-sapiens-uniprot-reviewed-contaminants-decoy-202210.fasta"),
-            "peptides": datafile("PXD017834-peptides.csv"),
-            "enzyme": "Trypsin",
-            "normalize": True,
-            "min_aa": 7,
-            "max_aa": 30,
-            "tpa": True,
-            "ruler": True,
-            "ploidy": 2,
-            "cpc": 200,
-            "organism": "human",
-            "output": datafile("PXD017834-norm.csv"),
-            "verbose": True,
-            "qc_report": datafile("QCprofile.pdf"),
-        }
-        print(args)
-        peptides_to_protein(**args)
+def test_ibaq_compute():
+    args = {
+        "fasta": "tests/example/Homo-sapiens-uniprot-reviewed-contaminants-decoy-202210.fasta",
+        "peptides": "tests/example/PXD017834-peptides.csv",
+        "enzyme": "Trypsin",
+        "normalize": True,
+        "min_aa": 7,
+        "max_aa": 30,
+        "tpa": True,
+        "ruler": True,
+        "ploidy": 2,
+        "cpc": 200,
+        "organism": "human",
+        "output": "tests/example/PXD017834-norm.csv",
+        "verbose": True,
+        "qc_report": "tests/example/QCprofile.pdf",
+    }
+    print(args)
+    peptides_to_protein(**args)
